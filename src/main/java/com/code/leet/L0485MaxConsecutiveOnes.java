76d149bf705f30bd1a3e014bd3181e686313016d
hejiaxu <316636073@qq.com>
2017-08-24 11:53:08 +0800

    maxOne

--- src/main/java/com/code/leet/L0485MaxConsecutiveOnes.java
+++ src/main/java/com/code/leet/L0485MaxConsecutiveOnes.java
@@ -1,4 +1,22 @@
 package com.code.leet;
 
+/*
+Given a binary array, find the maximum number of consecutive 1s in this array.
+
+ */
 public class L0485MaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                max = max > end - start ? max : end - start;
+                start = i + 1;
+            }
+            end++;
+        }
+        return max > end - start ? max : end - start;
+    }
+
 }
