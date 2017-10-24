fe3b4401a2797945e36d583afc0429554c289e73
hejiaxu <316636073@qq.com>
2017-10-24 19:00:17 +0800

    l0039

---
+++ src/main/java/com/code/leet/L0039CombinationSum.java
@@ -0,0 +1,68 @@
+package com.code.leet;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+
+Given a set of candidate numbers (C) (without duplicates) and a target number (T),
+find all unique combinations in C where the candidate numbers sums to T.
+
+The same repeated number may be chosen from C unlimited number of times.
+
+Note:
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+For example, given candidate set [2, 3, 6, 7] and target 7,
+ */
+public class L0039CombinationSum {
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        return combinationSumRecur(candidates, 0, target);
+    }
+
+    public List<List<Integer>> combinationSumRecur(int[] candidates, int pos, int target) {
+        List<List<Integer>> r = new LinkedList <>();
+        if (0 == target) {
+            LinkedList <Integer> e = new LinkedList <>();
+            r.add(e);
+            return r;
+        }
+
+        if (pos >= candidates.length) {
+            return null;
+        }
+
+        for (int i = 0; i * candidates[pos] <= target; i++) {
+            List <List <Integer>> lists = combinationSumRecur(candidates, pos + 1, target - candidates[pos] * i);
+            if ( lists != null ) {
+                for (List <Integer> list: lists){
+                    for (int j = 0; j < i; j++) {
+                        list.add(0, candidates[pos]);
+                    }
+                }
+                r.addAll(lists);
+            }
+        }
+
+        return r;
+    }
+
+    public static void main(String[] args) {
+//        int[] nums = {1, 1, 3, 4, 3};
+        int[] nums = {2, 3, 6, 7};
+//        int[] nums = {1, 3, 5, 6, 3, 3, 2, 1, 7, 4, 4};
+        int target = 7;
+        List <List <Integer>> lists = new L0039CombinationSum().combinationSum(nums, target);
+        for (int i = 0; i < lists.size(); i++) {
+            List <Integer> list = lists.get(i);
+            for (int j = 0; j < list.size(); j++) {
+                Integer integer = list.get(j);
+                System.out.print(integer);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
---
+++ src/main/java/com/code/leet/L0077Combinations.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package com.code.leet;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+ */
+
+public class L0077Combinations {
+
+	public static void main(String[] args) {
+		// TODO 
+		List<List<Integer>> combine = new L0077Combinations().combine(4, 2);
+		for (Iterator iterator = combine.iterator(); iterator.hasNext();) {
+			List<Integer> list = (List<Integer>) iterator.next();
+			for (Iterator iterator2 = list.iterator(); iterator2.hasNext();) {
+				Integer integer = (Integer) iterator2.next();
+				System.out.print(integer);
+			}
+			System.out.println();
+		}
+	}
+    //12ms
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans = new LinkedList();
+        if (n < k || k == 0) return ans;
+
+        ans = combine(n-1, k-1);
+        //if at this point ans is empty, it can only be that k-1 == 0,
+        //n-1<k-1 is not possible since n>=k (if n<k, the function would have already returned at an early point)
+        if (ans.isEmpty()) {
+            ans.add(new LinkedList<Integer>());
+        }
+        for (List<Integer> list:ans) {
+            list.add(n);
+        }
+
+        ans.addAll(combine(n-1, k));
+        return ans;
+    }
+
+}
